ed6ccb8ac6665f8aec7dbb5c515e27b80a99c0ed
izdeby <iuriiz@fb.com>
2019-06-07 10:37:39 -0700

    Intoducing bfloat16 type
    gh-metadata: pytorch pytorch 21522 gh/izdeby/8/head

---
+++ c10/util/BFloat16-inl.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <c10/macros/Macros.h>
+
+namespace c10 {
+
+using namespace std;
+
+/// Constructors
+
+inline C10_HOST_DEVICE BFloat16::BFloat16(float value) {
+  uint32_t res;
+  std::memcpy(&res, &value, sizeof(res));
+  val_ = res >> 16;
+}
+
+/// Implicit conversions
+inline C10_HOST_DEVICE BFloat16::operator float() const {
+  float res = 0;
+  uint32_t tmp = val_;
+  tmp <<= 16;
+  std::memcpy(&res, &tmp, sizeof(tmp));
+  return res;
+}
+
+} // namespace c10
